9ba78acbf913c0b697144eccb1d92c59a80fe406
Paul Maxim <collermanjohn@gmail.com>
2020-04-08 13:27:30 +0300

    Lab 8
    Lab 8

---
+++ Lab8TSP/GrpcGreeterClient/GrpcGreeterClient/Program.cs
@@ -0,0 +1,25 @@
+﻿using Grpc.Net.Client;
+using GrpcGreeter;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace GrpcGreeterClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var httpClientHandler = new HttpClientHandler();
+            httpClientHandler.ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+            var httpClient = new HttpClient(httpClientHandler);
+            var channel = GrpcChannel.ForAddress("https://localhost:5001", new GrpcChannelOptions { HttpClient = httpClient });
+            var client = new Greeter.GreeterClient(channel);
+            var reply = await client.SayHelloAsync(
+                              new HelloRequest { Name = "GreeterClient" });
+            Console.WriteLine("Greeting: " + reply.Message);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
