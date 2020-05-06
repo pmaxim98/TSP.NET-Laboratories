84e4d2336c063b8429ab7727a74e8c1cef493ca2
Paul Maxim <collermanjohn@gmail.com>
2020-05-06 13:31:00 +0300

    Lab 10
    Lab 10

---
+++ Lab10TSP/Asp_Maxim_Paul_Rp/Data/Asp_Maxim_Paul_RpContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Asp_Maxim_Paul_Rp.Models;
+
+namespace Asp_Maxim_Paul_Rp.Data
+{
+    public class Asp_Maxim_Paul_RpContext : DbContext
+    {
+        public Asp_Maxim_Paul_RpContext (DbContextOptions<Asp_Maxim_Paul_RpContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Asp_Maxim_Paul_Rp.Models.Movie> Movie { get; set; }
+    }
+}
