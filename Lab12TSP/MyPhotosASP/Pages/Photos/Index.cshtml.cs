9c6d9a5190cee6ad4d3f1201e517a83a62a82f71
Paul Maxim <collermanjohn@gmail.com>
2020-05-20 13:33:04 +0300

    Lab 12
    Lab 12

---
+++ Lab12TSP/MyPhotosASP/Pages/Photos/Index.cshtml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ServiceReferenceMyPhotos;
+
+namespace MyPhotosASP.Pages.Photos
+{
+    public class IndexModel : PageModel
+    {
+        private MyPhotosServiceClient service = new MyPhotosServiceClient();
+
+        private readonly IWebHostEnvironment hostEnvironment;
+
+        public List<MultimediaDTO> Multimedias { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+
+        public IndexModel(IWebHostEnvironment environment)
+        {
+            hostEnvironment = environment;
+
+            Multimedias = new List<MultimediaDTO>();
+        }
+
+        public async Task OnGetAsync()
+        {
+            List<MultimediaDTO> everyMultimedia = await service.GetEveryMultimediaAsync();
+
+            if (!string.IsNullOrEmpty(SearchString))
+                everyMultimedia = everyMultimedia.Where(s => s.Path.Contains(SearchString)).ToList();
+
+            foreach (MultimediaDTO multimedia in everyMultimedia)
+            {
+                var uploads = Path.Combine(hostEnvironment.WebRootPath, "uploads");
+                var filePath = Path.ChangeExtension(Path.Combine(uploads, multimedia.Id.ToString()), Path.GetExtension(multimedia.Path));
+
+                if (!System.IO.File.Exists(filePath))
+                    System.IO.File.Copy(multimedia.Path, filePath);
+
+                Multimedias.Add(multimedia);
+            }
+        }
+
+        public string GetUploadPath(MultimediaDTO multimedia)
+        {
+            return "uploads/" + multimedia.Id + Path.GetExtension(multimedia.Path);
+        }
+
+        public string GetNameFromPath(MultimediaDTO multimedia)
+        {
+            return Path.GetFileName(multimedia.Path);
+        }
+    }
+}
